6ac638dfacf5b32164925bd5dae7809b8d387417
Duale Siad <me@wale.id.au>
2023-07-02 02:14:25 +1000

    chore: amend build tests

--- plugin/src/test/kotlin/au/id/wale/tailwind/test/TailwindPluginTest.kt
+++ plugin/src/test/kotlin/au/id/wale/tailwind/test/TailwindPluginTest.kt
@@ -3,12 +3,17 @@ package au.id.wale.tailwind.test
 import au.id.wale.tailwind.TailwindExtension
 import au.id.wale.tailwind.tasks.TailwindDownloadTask
 import org.gradle.testfixtures.ProjectBuilder
+import org.gradle.testkit.runner.GradleRunner
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.io.TempDir
+import java.io.File
+import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
 class TailwindPluginTest {
     @Test
-    fun `plugin is applied correctly to the project`() {
+    fun `downloadTask is applied correctly to the project`() {
         val project = ProjectBuilder.builder().build()
         project.pluginManager.apply("au.id.wale.tailwind")
 
@@ -16,7 +21,7 @@ class TailwindPluginTest {
     }
 
     @Test
-    fun `properly download plugin`() {
+    fun `check if parameters are passed correctly to plugin`() {
         val project = ProjectBuilder.builder().build()
         project.pluginManager.apply("au.id.wale.tailwind")
 
@@ -25,8 +30,12 @@ class TailwindPluginTest {
             this.config.set("src/main/resources/tailwind.config.js")
             this.input.set("src/app/tailwind.css")
             this.output.set("${project.buildDir}/assets/app.css")
-        }.run {  }
+        }
 
-        assertTrue { project.state.executed }
+        val ext = project.extensions.getByName("tailwind") as TailwindExtension
+
+        assertEquals("src/main/resources/tailwind.config.js", ext.config.get())
+        assertEquals("src/app/tailwind.css", ext.input.get())
+        assertEquals("${project.buildDir}/assets/app.css", ext.output.get())
     }
 }
